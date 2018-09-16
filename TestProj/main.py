9d6f7a3cfd1e1257493d1cb416b01b12268da296
buttsj <buttsj@wit.edu>
2018-09-15 22:31:59 -0400

    tidy up code

--- TestProj/main.py
+++ TestProj/main.py
@@ -6,20 +6,19 @@ from phue import Bridge
 import spotipy.util as util
 from PIL import ImageFile
 
-username = ['h1z1vr98wjwqiyjfrs13bod9r', '[mag]intensity', 'user3', 'user4', 'user5']
+username = ['[USERNAME_0]', '[USERNAME_1]', '[USERNAME_2]', '[USERNAME_3]', '[USERNAME_4]'] # Usernames after 0 are optional.
 correctUser = ['', '']
 scopevals = ['user-read-playback-state,user-read-currently-playing,user-read-recently-played',
              'user-read-playback-state,user-read-currently-playing']
 scope = [scopevals[0], scopevals[1], scopevals[0], scopevals[1], scopevals[0]]
 token = util.prompt_for_user_token('', '', client_id='', client_secret='', redirect_uri='')
-b = Bridge('192.168.86.22')
+b = Bridge('[HUE_BRIDGE_IP]')
 spotify = ''
 old = set()
 prop_color = ['']
 
 def setup():
     global b
-    b = Bridge('192.168.86.22')
     b.set_light(1, 'on', True)
     b.set_light(2, 'on', True)
     ImageFile.LOAD_TRUNCATED_IMAGES = True
