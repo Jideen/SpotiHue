6affd3bbc9af53910e3bc1e24597bdb616902394
buttsj <buttsj@wit.edu>
2018-09-15 20:29:45 -0400

    Renamed to import to other files

--- TestProj/cover_art.py
+++ TestProj/cover_art.py
@@ -33,15 +33,13 @@ def theScript():
     global url
     global prop_color
     scope = 'user-read-currently-playing'
-    token = util.prompt_for_user_token('h1z1vr98wjwqiyjfrs13bod9r', scope, client_id='1abdcc884a4c4bf9b0f6ed7b65d07e07', client_secret='7ca1f32cbf004d34a10f4000e64e83d4',
-                                       redirect_uri='http://localhost/')
+    token = util.prompt_for_user_token('h1z1vr98wjwqiyjfrs13bod9r', scope, client_id='1abdcc884a4c4bf9b0f6ed7b65d07e07', client_secret='7ca1f32cbf004d34a10f4000e64e83d4', redirect_uri='http://localhost/')
     spotify = spotipy.Spotify(auth=token)
     current_track = spotify.currently_playing(market='US')
     result = json.dumps(current_track)
     stuff = re.findall(r'(https?://[^\s]+)', result)
-    removetable = str.maketrans('', '', '",')
-    out_list = [s.translate(removetable) for s in stuff]
-
+    removeTable = str.maketrans('', '', '",')
+    out_list = [s.translate(removeTable) for s in stuff]
     for i in range(1,10):
         global old
         url_check = out_list[i]
@@ -64,32 +62,26 @@ def theScript():
         else:
             newSong = True
     if "f0c53ef4d3" in str(url):
-        b.set_light(1, 'hue', 47104)
-        b.set_light(1, 'sat', 254)
-        b.set_light(1, 'bri', 72)
-        b.set_light(2, 'hue', 47104)
-        b.set_light(2, 'sat', 254)
-        b.set_light(2, 'bri', 72)
+        for i in range(1, 2):
+            b.set_light(i, 'hue', 47104)
+            b.set_light(i, 'sat', 254)
+            b.set_light(i, 'bri', 72)
         print("But soon comes mister night.")
         theScript()
     if "83c58aeccf0f8055" in url:
-        b.set_light(1, 'hue', 47104)
-        b.set_light(1, 'sat', 254)
-        b.set_light(1, 'bri', 72)
-        b.set_light(2, 'hue', 47104)
-        b.set_light(2, 'sat', 254)
-        b.set_light(2, 'bri', 72)
+        for i in range(1, 2):
+            b.set_light(i, 'hue', 47104)
+            b.set_light(i, 'sat', 254)
+            b.set_light(i, 'bri', 72)
         print("Reaching over, now his hand is on your shoulder.")
         theScript()
     if newSong:
-        b.set_light(1, 'hue', (int(prop_color[0] * 65535)))
-        b.set_light(1, 'bri', (int(prop_color[1] * 254)))
-        b.set_light(1, 'sat', (int(prop_color[2])))
-        b.set_light(2, 'hue', (int(prop_color[0] * 65535)))
-        b.set_light(2, 'bri', (int(prop_color[1] * 254)))
-        b.set_light(2, 'sat', (int(prop_color[2])))
+        for i in range(1,2):
+            b.set_light(i, 'hue', (int(prop_color[0] * 65535)))
+            b.set_light(i, 'bri', (int(prop_color[1] * 254)))
+            b.set_light(i, 'sat', (int(prop_color[2])))
+            # print(b.get_api())
         print("New song.")
-        # print(b.get_api())
         theScript()
     theScript()
 
